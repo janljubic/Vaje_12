33059516013a75862c4348395b55fa85eeeb09c9
janljubic <jan.ljubic@gmail.com>
2022-01-04 20:11:55 +0100

    change_1

---
+++ test.py
@@ -0,0 +1,14 @@
+def hello(name):
+    return "Hello " + name + "!"
+
+
+def racunanje(a, b): 
+    rez = a + b
+    rez = a * b
+    rez = a / b
+    
+    return rez
+
+print(hello("Jan"))
+print(racunanje(15, 15))
+
